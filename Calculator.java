f6666e3b2b60bded1f0c6e50765aa63497e54043
esakkimuthuselvi <37836220+esakkimuthuselvi@users.noreply.github.com>
2022-03-21 20:45:32 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -13,7 +13,14 @@ package week1.day2;
     public float getdivide(float num1, float num2) {
 		return num1/num2;
 	}
-    
+    public static void main(String args[]) {
+		Calculator nCalc= new Calculator();
+		//nCalc.getAdd(20, 34, 789);
+	    System.out.println(nCalc.getAdd(20, 34, 789));
+	    System.out.println(nCalc.getSub(560, 490));
+	    System.out.println(nCalc.getMul(5.3, 2));
+	    System.out.println(nCalc.getdivide(34.67f, 2.45f));
+    }
     
 }
 //public class MyCalculator{
